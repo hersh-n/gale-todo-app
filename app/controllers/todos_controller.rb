84abcd9ff5eacf716a39b0a1c063d6943d76b89d
Hershel Nashman <hershel.nashman@gmail.com>
2016-03-23 23:15:50 -0400

    create method updated, params instatiated

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,5 +8,14 @@ class TodosController < ApplicationController
     @todo = Todo.new
   end
 
+  def create
+    @todo = Todo.create todo_params
+     redirect_to action: 'index'
+  end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:title, :description, :priority, :status)
+    end
 
 end
