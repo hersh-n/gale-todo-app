c4d184be46d6d01740de3caeec8f1493de620bc6
Hershel Nashman <hershel.nashman@gmail.com>
2016-03-23 22:40:21 -0400

    method new added to controller and view

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,18 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+  end
+
+
+  private
+    def todo_params
+      params.require(:todo).permit(:title, :description)
+    end
+
 end
