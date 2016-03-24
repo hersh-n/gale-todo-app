55f5861af7a3ad816dd51d606c4e3f7bd50510d8
Hershel Nashman <hershel.nashman@gmail.com>
2016-03-23 22:26:51 -0400

    seed data created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+todos = Todo.create([
+  {title: "Buy Groceries", description: "Go to Safeway, don't forget the coupons."},
+
+  {title: "Wash the Car", description: "Have a date this Saturday."},
+
+  {title: "Clean my room", description: "It's that time of year again."},
+
+  {title: "Do the Laundry", description: "Don't forget the bleach!"},
+
+  {title: "Work on Mini-Project", description: "What are Happy Tails anyway?"},
+
+  {title: "Walk the Dog", description: "Take Fido around the block."}
+
+   ])
