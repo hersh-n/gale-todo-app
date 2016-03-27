409244737bf7cf08b47044b52a83c6bf5e3bfe13
Hershel Nashman <hershel.nashman@gmail.com>
2016-03-27 19:09:25 -0400

    seed date removed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-todos = Todo.create([
-  {title: "Buy Groceries", description: "Go to Safeway, don't forget the coupons.", priority: 1},
+# todos = Todo.create([
+#   {title: "Buy Groceries", description: "Go to Safeway, don't forget the coupons.", priority: 1},
 
-  {title: "Wash the Car", description: "Have a date this Saturday.", priority: 3},
+#   {title: "Wash the Car", description: "Have a date this Saturday.", priority: 3},
 
-  {title: "Clean my room", description: "It's that time of year again.", priority: 2},
+#   {title: "Clean my room", description: "It's that time of year again.", priority: 2},
 
-  {title: "Do the Laundry", description: "Don't forget the bleach!", priority: 3},
+#   {title: "Do the Laundry", description: "Don't forget the bleach!", priority: 3},
 
-  {title: "Work on Mini-Project", description: "What are Happy Tails anyway?", priority: 1},
+#   {title: "Work on Mini-Project", description: "What are Happy Tails anyway?", priority: 1},
 
-  {title: "Walk the Dog", description: "Take Fido around the block.", priority: 2}
+#   {title: "Walk the Dog", description: "Take Fido around the block.", priority: 2}
 
-   ])
+#    ])
